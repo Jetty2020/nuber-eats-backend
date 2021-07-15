3e0d66417252ecc3fad213f55d45aa1b8263c91e
Jetty2020 <kmclo@naver.com>
2021-07-15 17:31:50 +0900

    5.8 GraphQL Context

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation, Context } from '@nestjs/graphql';
 import {
   CreateAccountInput,
   CreateAccountOutput,
@@ -50,5 +50,11 @@ export class UserResolver {
   }
 
   @Query((returns) => User)
-  me() {}
+  me(@Context() context) {
+    if (!context.user) {
+      return;
+    } else {
+      return context.user;
+    }
+  }
 }
